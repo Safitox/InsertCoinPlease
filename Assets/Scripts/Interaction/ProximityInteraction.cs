b2f2bb6b0b767c510c6c6d128468605fecce724c
Sergio <checho1@gmail.com>
2025-09-01 14:01:32 -0300

    Interaction optional

--- Assets/Scripts/Interaction/ProximityInteraction.cs
+++ Assets/Scripts/Interaction/ProximityInteraction.cs
@@ -1,32 +1,45 @@
 using UnityEngine;
 
 
-public class ShowInteraction : MonoBehaviour
+
+public class ProximityInteraction : MonoBehaviour
 {
+    //UI Interacción   
     bool playerInRange = false;
     [SerializeField] Transform interactionUI;
+    [SerializeField] bool ShowVisualAid = true; //Por si se quiere mantener el trigger pero no mostrar la UI
 
 
+    private void Awake()
+    {
+        ShowVisual(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         playerInRange = other.CompareTag("Player");
-        interactionUI.gameObject.SetActive(true);
+        ShowVisual(true);
     }
 
     private void OnTriggerExit(Collider other)
     {
         playerInRange = !other.CompareTag("Player");
-        interactionUI.gameObject.SetActive(false);
+        ShowVisual(false);
     }
 
     private void FixedUpdate()
     {
+        //TODO: Optimizar usando corrutinas tal vez?
         if (playerInRange)
         {
             interactionUI.LookAt(Camera.main.transform);
             interactionUI.Rotate(0, 180, 0);
         }
         
+    }
+
+    void ShowVisual(bool show)
+    {
+        interactionUI.gameObject.SetActive(show && ShowVisualAid);
     }
 
 }
