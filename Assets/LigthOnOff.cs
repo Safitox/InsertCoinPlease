3f3a89033c3f27571a5747c6b7c0b67c5430efe5
Mati028as <matiassis2017@gmail.com>
2025-09-05 23:12:22 -0300

    MatiScene

---
+++ Assets/LigthOnOff.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class LigthOnOff : MonoBehaviour
+{
+    [SerializeField] private InteractionObject buttonLigth;
+
+    [SerializeField] private Light redLigth;
+    [SerializeField] private Light greenLigth;
+
+    [SerializeField] private Renderer redRenderer;
+    [SerializeField] private Renderer greenRenderer;
+
+    [SerializeField] private Color greenEmissionOn = Color.green;
+    [SerializeField] private Color emissionOff = Color.black;
+
+    void Update()
+    {
+        if (buttonLigth.press)
+        {
+            if (redRenderer.material.HasProperty("_EmissionColor"))
+            {
+                redRenderer.material.SetColor("_EmissionColor", emissionOff);
+                redLigth.enabled = false;
+            }
+
+            if (greenRenderer.material.HasProperty("_EmissionColor"))
+            {
+                greenRenderer.material.SetColor("_EmissionColor", greenEmissionOn);
+                greenLigth.enabled = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/Interaction/InteractionObject.cs
+++ Assets/Scripts/Interaction/InteractionObject.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class InteractionObject : MonoBehaviour
 {
     public float TimeToExecute ;
+    public bool press;
 
     //TODO: Cambiar a abstract!
     public virtual void Interact()
     {
+        press = true;
         Debug.Log("Presionado");
     }
 }
