69e75f1ad2e348ebc2795f94b1aa5d2d7ed3df54
Emanuel Gomez <48289234+Emakin@users.noreply.github.com>
2025-09-20 19:34:06 -0300

    Modificando escena e intentando cosas
    Modifique mi escena para que sea un poco más elaborado e intente hacer un sistema de vida para el jugador el cual consistia en mostrar en pantalla un glitch pero fracase por lo cual deje todo código del sistema comentado

--- Assets/Scripts/Healt/HealthManager.cs
+++ Assets/Scripts/Healt/HealthManager.cs
@@ -7,6 +7,8 @@ public class HealthManager : MonoBehaviour, IHeathManager
     public int Health { get { return health;} 
                         set { health = value; } }
 
+    //public GlitchManager glitch; Quise agregar un glicth que aparezca transicionando de invisible a visible por cada daño, pero no me salio
+
     public void Death()
     {
         //animación de muerte
@@ -18,11 +20,13 @@ public class HealthManager : MonoBehaviour, IHeathManager
     public void TakeDamege(int damage)
     {
         health -= damage;
+        float healthPercent = health / 100;
         Debug.Log(transform.name + " recibió daño. Vida actual: " + health);
         if (health <= 0)
         {
             Death();
         }
+        //glitch.UpdateGlitch(healthPercent);
 
     }
 
