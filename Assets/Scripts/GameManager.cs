e106a511ce5a6bf68671a24b170425a85df9ad74
Emanuel Gomez <48289234+Emakin@users.noreply.github.com>
2025-09-25 22:55:22 -0300

    Vuelvo a un commit anterior
    El merge con main rompe la mecanica de Mati, asi que vuelvo a un commit anterior donde me funcionaba

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,4 +4,25 @@ public class GameManager : Singleton<GameManager>
 {
     public Transform player;
     public int lives = 3;
+
+    private void Awake()
+    {
+        ServiceLocator.Instance.RegisterService (new ResourceLoader());
+    }
+
+
+    public void LoseLife()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            Debug.Log("Lives left: " + lives);
+        }
+    }
+
+
 }
